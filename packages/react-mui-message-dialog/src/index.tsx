c3575193699d3b5a25fcefad0e00fb4669337dc5
jhelom <1127424+jhelom@users.noreply.github.com>
2025-06-19 21:56:09 +0900

    chore: format exports in index.tsx for consistency

--- packages/react-mui-message-dialog/src/index.tsx
+++ packages/react-mui-message-dialog/src/index.tsx
@@ -1,3 +1,3 @@
-export {MessageDialogProvider} from './MessageDialog';
-export {useMessageDialog} from './MessageDialogContext';
-export {MessageDialogSettings} from './MessageDialog';
+export { MessageDialogProvider } from './MessageDialog';
+export { useMessageDialog } from './MessageDialogContext';
+export type { MessageDialogSettings } from './MessageDialog';
